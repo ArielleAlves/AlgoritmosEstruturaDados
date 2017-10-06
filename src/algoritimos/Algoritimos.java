9bec199a1b6d71cb8e92cd6a0c12445ae307da80
Aluno <Aluno@172.32.1.21>
2017-10-05 21:49:55 -0300

    calculando metade raiz e quadrado Adicionando outra classe

--- src/algoritimos/Algoritimos.java
+++ src/algoritimos/Algoritimos.java
@@ -1,10 +1,29 @@
 package algoritimos;
 
 public class Algoritimos {
+    
+    public static void quadrado(double n){
+        double r = n*n;    
+        
+        System.out.println("Quadrado é "+r);
+    }
+    
+    public static double metade(double v){
+        double r=  v/2;
+        
+        return r;
+    }
 
     public static void main(String[] args) {
+        System.out.println("Incicio");
+        //calculando a raiz quadrada
+        double raiz;
+        raiz = Math.sqrt(15);
 
-
+        System.out.println("A raiz é "+raiz);
+        
+        System.out.println("Metade"+metade(15));
+        quadrado(15);
     }
     
 }
